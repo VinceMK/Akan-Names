f62550034cc05b4ac169a6dc3a4581be0c871eb1
VinceMK <vincent.mwangi@student.moringaschool.com>
2021-04-14 12:16:59 +0300

    Control flow logic for thr day of birth calculation

--- main.js
+++ main.js
@@ -1,15 +1,44 @@
 function getBirthday() {
     alert("Enter Your date of Birth to get Akan name");
     let dob = prompt("Enter Date of Birth mm/dd/yyyy");
+    let gender = prompt("Enter gender 'M' for male, 'F' for female");
+
+    getAkanName(dob, gender)
+
+}
+
+function getAkanName(dob, gender) {
     let dateStr = new Date(dob);
-    let [month, date, year] = dateStr.toLocaleDateString("en-US").split("/");
-    let gender = prompt("Enter gender 'M' for male, 'F' for female")
-    let CC = year[0, 1]
+    let dayofWeek = dateStr.getDay();
+    let male = [
+        " Sunday: Kwasi ",
+        "Monday: Kwadwo",
+        "Tuesday: Kwabena",
+        "Wednesday: Kwaku",
+        "Thursday: Yaw",
+        "Friday: Kofi",
+        "Saturday: Kwame"
+    ];
+    let female = [
+        "Sunday: Akosua",
+        "Monday: Adwoa",
+        "Tuesday: Abenaa",
+        "Wednesday: Akua",
+        "Thursday: Yaa",
+        "Friday: Afua",
+        "Saturday: Ama"
+    ];
+    if (gender == "M") {
+        let akan[] = male[dayofWeek];
+        alert("Since you were born on")
 
 
+    } else if (gender == "F") {
+        console.log(female[dayofWeek]);
+
+    } else {
 
 
-
-    console.log(CC);
+    }
 
 }
